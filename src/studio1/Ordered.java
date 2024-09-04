6d3094667515de5c9d99c6dca950e417148eec91
prestonlopez <prestonlopez@localhost>
2024-09-04 15:23:29 -0500

    Finished studio 1 !!

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -10,12 +10,15 @@ public class Ordered {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Value for x?");
+		boolean isOrdered;
 		int x = in.nextInt();
 		System.out.println("Value for y?");
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
-	}
+		isOrdered=((x>=y&&y>=z)||(x<=y&&y<=z));
+		System.out.println(isOrdered);
+		
+		}
 
 }
